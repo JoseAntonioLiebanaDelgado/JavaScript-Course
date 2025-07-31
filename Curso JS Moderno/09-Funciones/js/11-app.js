e6e194cea2b7d62333bb5f3246d2846a69cf4e87
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2025-07-31 21:08:30 +0200

    Functions
    68. The Advantages of Arrow Functions

---
+++ Curso JS Moderno/09-Funciones/js/11-app.js
@@ -0,0 +1,13 @@
+// 68. Las Ventajas de los Arrow Functions
+
+// Parametros
+const aprendiendo = (tecnologia) => console.log(`aprendiendo ${tecnologia}`);
+aprendiendo('JavaScript');
+
+// si es un solo parmetro no ocupamos el parentesis
+const aprendiendo = tecnologia => console.log(`aprendiendo ${tecnologia}`);
+aprendiendo('JavaScript');
+
+// multiples parametros si requieren parentesis
+const aprendiendo = (tecn1, tecn2) => console.log(`Aprendiendo ${tecn1} ${tecn2}`);
+aprendiendo('JS', 'ES');
