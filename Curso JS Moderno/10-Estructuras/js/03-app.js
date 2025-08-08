7ce9db9542493270250e69102d892515057a1d24
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2025-08-08 15:03:35 +0200

    Structures
    73. Comparing Greater than or Less than

---
+++ Curso JS Moderno/10-Estructuras/js/03-app.js
@@ -0,0 +1,19 @@
+// 73. Comparando Mayor o Menor que
+
+let dinero = 500;
+let totalCarrito = 300;
+
+if( dinero > totalCarrito ) {
+    console.log('Pago Correcto');
+} else {
+    console.log('Fondos Insuficientes');
+}
+
+// También puede ser sin llaves
+let dinero = 500;
+let totalCarrito = 300;
+
+if( dinero > totalCarrito )
+    console.log('Pago Correcto');
+else
+    console.log('Fondos Insuficientes');
