824274cbdbd5b18252c0136e26ea11cff4a707d6
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2025-08-22 08:34:07 +0200

    Structures
    79. The Ternary Operator

---
+++ Curso JS Moderno/10-Estructuras/js/09-app.js
@@ -0,0 +1,31 @@
+// 79. El Operador Ternario
+
+const edad = 18;
+const esMayorDeEdad = (edad >= 18) ? "Es mayor de edad" : "Es menor de edad";
+console.log(esMayorDeEdad);
+
+
+//---
+
+
+
+// Previamente vimos los operadores ternarios, llegan a ser muy útiles ya que te dan un código más simplificado, veamos como se utilizan...
+
+const autenticado = false;
+const puedePagar = false
+
+console.log( autenticado  ? 'Si esta autenticado' : 'No esta autenticado');
+
+
+// El OR y el && también se pueden utilizar en el ternario
+
+console.log( autenticado && puedePagar ? 'Si esta autenticado' : 'No esta autenticado');
+
+// Ternario anidado...
+
+console.log( autenticado  ?  puedePagar ? 'Si puede pagar' : 'esta autenticado pero no puede pgar' : 'No esta autenticado');
+
+
+// Jugar con los valores...
+
+// En el siguiente capítulo vamos a ver los iteradores, en JavaScript hay varios, así que veamos algunos ejemplos...
