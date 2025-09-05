3431705cbab8c09f2376ab696041726588955a53
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2025-09-05 08:03:43 +0200

    ArrayMethods
    88. .some

--- Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -1,10 +1,32 @@
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+// 88. .some
+
+const pendientes = ['Tarea', 'Comer', 'Proyecto', 'Estudiar JS'];
 
 const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
-    { nombre: 'Tablet', precio: 200 },
-    { nombre: 'Audifonos', precio: 300 },
-    { nombre: 'Teclado', precio: 400 },
-    { nombre: 'Celular', precio: 700 },
-]
+    {id: 1, producto: 'Libro', precio: 20 },
+    {id: 2, producto: 'Camisa', precio: 10},
+    {id: 3, producto: 'Disco', precio: 300},
+    {id: 4, producto: 'Guitarra', precio: 500},
+    {id: 5, producto: 'Bateria', precio: 700},
+    {id: 6, producto: 'Piano', precio: 1000},
+];
+
+// Comprobar si al menos un elemento cumple con una condición
+
+let resultado = carrito.some( producto => producto.precio > 400 );
+console.log(resultado);
+
+resultado = carrito.some( producto => producto.precio > 1000 );
+console.log(resultado);
+
+// El some es ideal para cuando queremos comprobar si al menos un elemento cumple con una condición, en este caso si al menos un producto tiene un precio mayor a 400, si es así nos devolverá true, de lo contrario false.
+
+// De igual forma si queremos saber si al menos una tarea es igual a 'Comer'
+
+resultado = pendientes.some( pendiente => pendiente === 'Comer' );
+console.log(resultado);
+
+resultado = pendientes.some( pendiente => pendiente === 'Estudiar' );
+console.log(resultado);
+
+// Y con eso terminamos lo que son los iteradores, veamos una serie de Array Methods, que son similares a estos iteradores y sus usos. ya vamos terminando toda la parte de básicos de JavaScript para movernos al DOM pero vamos viendo un tema más!
