d1db4c9affffc829d8cf016dab0c78d0a09801e0
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2025-10-10 16:02:44 +0200

    JavaScript DOM
    99. getElementById

---
+++ Curso JS Moderno/13-DOM/js/03-scripts.js
@@ -0,0 +1,13 @@
+// 99. getElementById
+
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+// Si no exist eestará vacio
+
+const noExiste = document.getElementById('no-existe');
+console.log(noExiste);
+
+// Añadir un ID 2 veces, seleccionara el primero que encuentre...
+
+// Existen otro par de selectores que hoy en día son los más comunes, tienen una sintaxis muy similar a la de CSS, así que vamos viendolos en el siguiente video...
